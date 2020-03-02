e4c2b45e4d9fca75ae7b2cc2ec83b899f0a7d749
Lance Jared L.Cabiscuelas (KPPH- RPB9N0092D) <PB9N0092@calcomp.com.tw>
2020-03-02 14:56:36 +0800

    latest update installing chart in chatbot and survey pages

--- src/app/components/chatbot/chatbot.component.ts
+++ src/app/components/chatbot/chatbot.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatbotComponent implements OnInit {
 
+  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public doughnutChartData = [120, 150, 180, 90];
+  public doughnutChartType = 'doughnut';
+
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
